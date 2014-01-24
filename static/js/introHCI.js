8a54873732aa7a5dfe37ed3c3d84f4931ae6c604
vagrant <vagrant@precise32.(none)>
2014-01-24 04:55:58 +0000

    javascipt lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,43 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+        initializePage();
 })
 
 /*
- * Function that is called when the document is ready.
- */
+* Function that is called when the document is ready.
+*/
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+        $("#testjs").click(function(e) {
+                $('.jumbotron h1').text("All Work And No Play");
+                $("#testjs").text("Please wait...");
+                $(".jumbotron p").addClass("active");
+                $(".jumbotron p").toggleClass("active");
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+        });
+
+        // Add any additional listeners here
+        // example: $("#div-id").click(functionToCall);
+        $("a.thumbnail").click(projectClick);
+        $("#submitBtn").click(updateProject);
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + ".project-description").text(newText);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
 }
